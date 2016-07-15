acf6a3b87f1178de2776667dd019dafe121a0e62
David West <david.c.west@me.com>
2016-07-15 14:27:43 +0200

    Create create_pkg.sh
    Stealing auto-update-magic create_pkg.sh script to make it easier to deploy CasperCheck

---
+++ create_pkg.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+###
+#
+#            Name:  create_pkg.sh
+#     Description:  This script automatically creates a pkg file that you can
+#                   use to deploy the Casper Check script/LaunchDaemon
+#                   pair to your managed clients.
+#   Ripped off by:  David West 
+#          Author:  Elliot Jordan <elliot@lindegroup.com>
+#         Created:  2015-09-18
+#   Last Modified:  2016-07-15
+#         Version:  1.0
+#
+###
+
+cd "$(dirname "$0")"
+
+if [[ ! -f "./caspercheck.sh" ||
+      ! -f "./com.company.caspercheck.plist" ||
+      ! -f "./pkg_scripts/postinstall" ]]; then
+    echo "[ERROR] At least one required file is missing. Ensure that the following files exist in the folder:"
+    echo "    caspercheck.sh"
+    echo "    com.company.caspercheck.plist"
+    echo "    pkg_scripts/postinstall"
+    exit 1
+fi
+
+script_md5=$(md5 -q ./caspercheck.sh)
+if [[ "$script_md5" == "7b1cc4afd0f53484ca772c0ac06ee786" ]]; then
+    echo "[ERROR] It looks like you haven't customized the caspercheck.sh script yet. Please do that now, then run create_pkg.sh again."
+    exit 2
+fi
+
+echo "Great! Sounds like you're good to go."
+
+TMP_PKGROOT="/private/tmp/caspercheck/pkgroot"
+echo "Building package root in /tmp folder..."
+mkdir -p "$TMP_PKGROOT/Library/LaunchDaemons" "$TMP_PKGROOT/Library/Scripts"
+
+echo "Copying the files to the package root..."
+cp "./net.ripe.caspercheck.plist" "$TMP_PKGROOT/Library/LaunchDaemons/"
+cp "./caspercheck.sh" "$TMP_PKGROOT/Library/Scripts/"
+
+echo "Setting mode and permissions..."
+chown -R root:wheel "$TMP_PKGROOT"
+chmod +x "$TMP_PKGROOT/Library/Scripts/caspercheck.sh"
+
+echo "Building the package..."
+pkgbuild --root "/tmp/caspercheck/pkgroot" \
+         --scripts "./pkg_scripts" \
+         --identifier "net.ripe.caspercheck.plist" \
+         --version "2.0" \
+         --install-location "/" \
+         "./caspercheck-$(date "+%Y%m%d").pkg"
+
+exit 0
